6f245d5e0cead57bd3c08a46801e00f4b8257dd1
Luke Lee <7488977@qq.com>
2016-12-21 18:33:21 +0800

    格式化userdata.cs, 添加了3个尺寸的logo

--- userdata.cs
+++ userdata.cs
@@ -1,60 +1,58 @@
-﻿
-
-namespace 称重客户端New
+﻿namespace 称重客户端New
 {
-  internal class userdata
-  {
-    private static string url;
-    private static string token;
-    private static string state;
-    private static string id;
-
-    public static string URL
+    internal class userdata
     {
-      get
-      {
-        return userdata.url;
-      }
-      set
-      {
-        userdata.url = value;
-      }
-    }
+        private static string url;
+        private static string token;
+        private static string state;
+        private static string id;
 
-    public static string TOKEN
-    {
-      get
-      {
-        return userdata.token;
-      }
-      set
-      {
-        userdata.token = value;
-      }
-    }
+        public static string URL
+        {
+            get
+            {
+                return userdata.url;
+            }
+            set
+            {
+                userdata.url = value;
+            }
+        }
 
-    public static string STATE
-    {
-      get
-      {
-        return userdata.state;
-      }
-      set
-      {
-        userdata.state = value;
-      }
-    }
+        public static string TOKEN
+        {
+            get
+            {
+                return userdata.token;
+            }
+            set
+            {
+                userdata.token = value;
+            }
+        }
 
-    public static string ID
-    {
-      get
-      {
-        return userdata.id;
-      }
-      set
-      {
-        userdata.id = value;
-      }
+        public static string STATE
+        {
+            get
+            {
+                return userdata.state;
+            }
+            set
+            {
+                userdata.state = value;
+            }
+        }
+
+        public static string ID
+        {
+            get
+            {
+                return userdata.id;
+            }
+            set
+            {
+                userdata.id = value;
+            }
+        }
     }
-  }
 }
